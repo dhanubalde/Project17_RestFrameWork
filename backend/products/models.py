6103aa13ef7ead80629ff31d0138b95b6a0e120b
Dhan_ubalde <101233254+dhanubalde@users.noreply.github.com>
2023-08-20 20:50:53 +0800

    Update models.py
    change search manager

--- backend/products/models.py
+++ backend/products/models.py
@@ -33,7 +33,7 @@ class Product(models.Model):
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
     public = models.BooleanField(default=True)
-
+    objects = ProductManager()
     def __str__(self):
         return f"{self.title} -> {self.pk}"
 
