daa6220551f74bbf5e7c6281d91f73d4cacc2102
dhan <kedanubalde41@gmail.com>
2023-08-26 17:24:52 +0800

    add buildfiles and collect static files

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 
-python3.10 manage.py makemigrations
-python3.10 manage.py migrate
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
 
-python3.10 manage.py collectstatic
+python3.10 manage.py collectstatic --noinput
