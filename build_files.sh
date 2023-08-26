daaa0f2ec9a0113324bde0320005b157b04b70cf
dhan <kedanubalde41@gmail.com>
2023-08-26 16:55:39 +0800

    add buildfiles and collect static files

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
+echo  "Building a Project"
 python3.9 pip install -r requirements.txt
 
+echo "make a database migration"
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
+
+echo "collecting staticfiles"
+python3.9 manage.py collectstatic
