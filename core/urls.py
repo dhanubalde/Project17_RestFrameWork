86d667af1a22c36a74be2480d0a6e572ce19eeca
dhan <kedanubalde41@gmail.com>
2023-08-29 11:48:18 +0800

    add procfile to development

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api', include('api.urls')),
+    path('api/', include('api.urls')),
     path('api/search/', include('search.urls')),
     path('api/products/', include('products.urls')),
     path('api/v2/', include('core.routers'))
